ee01386441c7eb40e41d5eaa570ca3830c1c99e2
xia.zhang <xia.zhang@ibm.com>
2017-10-01 11:14:54 -0500

    Create new cities

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 app.use(express.static('public'));
+
+var cities = {
+  'Lotopia': 'some description',
+  'Caspiana': 'description',
+  'Indigo': 'description'
+};
+var urlencode = bodyParser.urlencoded({ extended: false });
 
 app.get('/', function (request, response) {
   response.send('OK');
 });
 
 app.get('/cities', function (request, response) {
-  var cities = ['Lotopia', 'Caspiana', 'Indigo'];
-  response.json(cities);
+  response.json(Object.keys(cities));
+});
+
+app.post('/cities', urlencode, function (request, response) {
+  var newCity = request.body;
+  cities[newCity.name] = newCity.description;
+  response.status(201).json(newCity.name);
 });
 
 module.exports = app;
